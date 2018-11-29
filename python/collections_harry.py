c22c77d99abb2f9d098eecaab27199fbd74f1ee8
zkan <kan@prontomarketing.com>
2018-11-29 07:06:47 +0700

    Use defaultdict to set default value for not-yet-existing key :bear:

--- python/collections_harry.py
+++ python/collections_harry.py
@@ -1,6 +1,27 @@
-from collections import Counter
+from collections import (
+    Counter,
+    defaultdict,
+)
 
 
+# defaultdict
+company = {
+    'name': 'Pronto Tools'
+}
+try:
+    company['developers'].append('Kan')
+except KeyError:
+    print("KeyError: 'developers'")
+
+
+company = defaultdict(list)
+company['name'] = 'Pronto Tools'
+company['engineers'].append('Kan')
+company['engineers'].append('Mils')
+print(company)
+
+
+# Counter
 file_name = 'harry.txt'
 
 with open(file_name) as f:
@@ -8,3 +29,6 @@ with open(file_name) as f:
     words = text.split(' ')
     c = Counter(words)
     print(f'Top 5 common words: {c.most_common(5)}')
+
+
+
