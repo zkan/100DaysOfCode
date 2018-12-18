6c895888f512186b3019490960e9740ee732032e
zkan <kan@prontomarketing.com>
2018-12-18 07:43:05 +0700

    Play with ValueError :bear:

--- python/error_handling/main.py
+++ python/error_handling/main.py
@@ -7,3 +7,17 @@ try:
     print(x)
 except NameError:
     print('We need to define the variable before use')
+
+
+def get_user_input():
+    user_input = input('Enter something: ')
+    if not user_input:
+        raise ValueError('Input cannot be empty')
+
+    return user_input
+
+
+try:
+    print(get_user_input())
+except ValueError as e:
+    print(f'ERROR: {e}')
