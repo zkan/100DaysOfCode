308ae440b44da57579e4c8942450f5f5df5ad46f
zkan <kan@prontomarketing.com>
2018-12-28 07:13:53 +0700

    Read CSV file :bear:

---
+++ python/csv/program.py
@@ -0,0 +1,13 @@
+import research
+
+
+def main():
+    research.init()
+
+    print('Weather reearch for Seattle, 2014-2015')
+    print('The hottest 5 days:')
+    print('The coldest 5 days:')
+    print('The wettest 5 days:')
+
+if __name__ == '__main__':
+    main()
---
+++ python/csv/research.py
@@ -0,0 +1,12 @@
+import os
+
+
+data = []
+
+
+def init():
+    base_folder = os.path.dirname(__file__)
+    filename = os.path.join(base_folder, 'data', 'seattle.csv')
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+        print(fin.read())
