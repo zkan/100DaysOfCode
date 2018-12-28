6caeca9e3a827df521ece806875c4bad8b4abaf7
zkan <kan@prontomarketing.com>
2018-12-28 07:25:34 +0700

    Use namedtuple to store data :bear:

--- python/csv/program.py
+++ python/csv/program.py
@@ -4,10 +4,6 @@ import research
 def main():
     research.init()
 
-    print('Weather reearch for Seattle, 2014-2015')
-    print('The hottest 5 days:')
-    print('The coldest 5 days:')
-    print('The wettest 5 days:')
 
 if __name__ == '__main__':
     main()
--- python/csv/research.py
+++ python/csv/research.py
@@ -1,7 +1,13 @@
+import collections
+import csv
 import os
 
 
 data = []
+Record = collections.namedtuple(
+    'Record',
+    'actual_mean_temp,actual_min_temp',
+)
 
 
 def init():
@@ -9,4 +15,17 @@ def init():
     filename = os.path.join(base_folder, 'data', 'seattle.csv')
 
     with open(filename, 'r', encoding='utf-8') as fin:
-        print(fin.read())
+        reader = csv.DictReader(fin)
+
+        for row in reader:
+            record = parse_row(row)
+            print(record)
+
+
+
+def parse_row(row):
+    record = Record(
+        actual_mean_temp=row.get('actual_mean_temp'),
+        actual_min_temp=row.get('actual_min_temp')
+    )
+    return record
