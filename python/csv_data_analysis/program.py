ba6b12e7d1304cddc90a2b84dfb3064ee955a6e4
zkan <kan@prontomarketing.com>
2018-12-30 07:26:32 +0700

    Find max fatal accidents and max fatalities :bear:

--- python/csv_data_analysis/program.py
+++ python/csv_data_analysis/program.py
@@ -12,6 +12,10 @@ Record = collections.namedtuple(
 
 
 def parse_row(row):
+    for k, v in row.items():
+        if k != 'airline':
+            row[k] = int(v)
+
     record = Record(**row)
     return record
 
@@ -19,11 +23,37 @@ def parse_row(row):
 def main():
     base_path = Path('.')
     filename = base_path / 'data' / 'airline-safety.csv'
+    records = []
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             record = parse_row(row)
             print(record)
+            records.append(record)
+
+    print()
+
+    max_fatal_accidents_85_99 = max(
+        records,
+        key=lambda x: x.fatal_accidents_85_99
+    )
+    print('Max fatal accidents in 85-99: ', max_fatal_accidents_85_99)
+    max_fatalities_85_99 = max(
+        records,
+        key=lambda x: x.fatalities_85_99
+    )
+    print('Max fatalities in 85-99: ', max_fatalities_85_99)
+
+    max_fatal_accidents_00_14 = max(
+        records,
+        key=lambda x: x.fatal_accidents_00_14
+    )
+    print('Max fatal accidents in 00-14: ', max_fatal_accidents_00_14)
+    max_fatalities_00_14 = max(
+        records,
+        key=lambda x: x.fatalities_00_14
+    )
+    print('Max fatalities in 00-14: ', max_fatalities_00_14)
 
 
 if __name__ == '__main__':
