3baacd809dd1cb159b6197d4f72a40b175ddb562
zkan <kan@prontomarketing.com>
2019-01-04 08:07:20 +0700

    Open browser to view talk python episode :bear:

--- python/search_api/program.py
+++ python/search_api/program.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 import api
 
 
@@ -9,7 +11,17 @@ def main():
     results = api.find_talk_python_episodes(keywords)
     print(f'There are {len(results)} matching episodes:')
     for index, each in enumerate(results, 1):
-        print(f'{index}. {each.title} ({each.url})')
+        print(f'{index}. [Episode {each.id}] {each.title} ({each.url})')
+
+    print()
+    episode_urls = {index: each.url for index, each in enumerate(results, 1)}
+    number = int(input('Pick the episode number to listen: '))
+    url = f'https://talkpython.fm{episode_urls[number]}'
+
+    # If new is 0, the url is opened in the same browser window if possible.
+    # If new is 1, a new browser window is opened if possible.
+    # If new is 2, a new browser page (“tab”) is opened if possible.
+    webbrowser.open(url, new=2)
 
 
 if __name__ == '__main__':
