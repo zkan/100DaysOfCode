4daa337e8fcc70103b4d6934362ccd8df0a01d97
zkan <kan@prontomarketing.com>
2019-01-15 06:19:20 +0700

    Use uplink to call APIs to get the data :bear:

--- python/search_api_with_uplink/api.py
+++ python/search_api_with_uplink/api.py
@@ -1,1 +1,19 @@
 import uplink
+
+
+class MovieSearchClient(uplink.Consumer):
+
+    def __init__(self):
+        super().__init__(base_url='http://movie_service.talkpython.fm')
+
+    @uplink.get('/api/search/{keyword}')
+    def search_movies(self, keyword):
+        """ Search movies with keyword """
+
+    @uplink.get('/api/director/{director_name}')
+    def search_movies_by_director(self, director_name):
+        """ Search movies by director """
+
+    @uplink.get('/api/movie/{imdb_number}')
+    def get_movie_by_imdb(self, imdb_number):
+        """ Get movie by IMDB code """
--- python/search_api_with_uplink/program.py
+++ python/search_api_with_uplink/program.py
@@ -2,7 +2,19 @@ import api
 
 
 def main():
-    pass
+    client = api.MovieSearchClient()
+    response = client.search_movies('game')
+    print(response.json())
+
+    print()
+
+    response = client.search_movies_by_director('steven')
+    print(response.json())
+
+    print()
+
+    response = client.get_movie_by_imdb('tt0181689')
+    print(response.json())
 
 
 if __name__ == '__main__':
