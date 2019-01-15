d9d65e4ccf807d43702650ca7fba3aa9e95af190
zkan <kan@prontomarketing.com>
2019-01-16 06:50:38 +0700

    Allow user to search by her choice :bear:

--- python/search_api_with_uplink/program.py
+++ python/search_api_with_uplink/program.py
@@ -3,18 +3,36 @@ import api
 
 def main():
     client = api.MovieSearchClient()
-    response = client.search_movies('game')
-    print(response.json())
 
-    print()
+    while True:
+        question = 'How do you want to search the movies ' \
+            '1) by keyword 2) by director 3) by IMDB code? '
+        user_choice = int(input(question))
 
-    response = client.search_movies_by_director('steven')
-    print(response.json())
+        results = []
+        if user_choice == 1:
+            keyword = input('Enter your keyword: ')
+            response = client.search_movies(keyword)
+            results = response.json()['hits']
+        elif user_choice == 2:
+            director_name = input("Enter a director's name: ")
+            response = client.search_movies_by_director(director_name)
+            results = response.json()['hits']
+        elif user_choice == 3:
+            imdb_code = input("Enter a IMDB code: ")
+            response = client.get_movie_by_imdb(imdb_code)
+            results.append(response.json())
+        else:
+            print('See you next time!')
+            break
 
-    print()
+        for each in results:
+            print()
+            print(f"IMDB Code: {each['imdb_code']}")
+            print(f"Title: {each['title']}")
+            print(f"Director: {each['director']}")
 
-    response = client.get_movie_by_imdb('tt0181689')
-    print(response.json())
+        print()
 
 
 if __name__ == '__main__':
