68772a1eed9593d5600be65b7bfbd284d947f749
zkan <kan@prontomarketing.com>
2019-01-20 07:28:22 +0700

    Get repos and sort them by number of stars :bear:

--- python/github_api/github_api.py
+++ python/github_api/github_api.py
@@ -1,4 +1,6 @@
-from github import Github, InputFileContent
+from dataclasses import dataclass
+
+from github import Github
 
 
 gh = Github()
@@ -6,3 +8,26 @@ print(gh.rate_limiting)
 
 zkan = gh.get_user('zkan')
 print(zkan)
+
+repos = zkan.get_repos()
+print(repos)
+
+
+@dataclass
+class Repo:
+    name: str
+    stars: int
+    forks: int
+
+
+repos = []
+for each in zkan.get_repos():
+    if each.fork:
+        continue
+
+    repos.append(Repo(name=each.name,
+                      stars=each.stargazers_count,
+                      forks=each.forks_count))
+
+sorted_repos = sorted(repos, key=lambda x: x.stars, reverse=True)[:5]
+print(sorted_repos)
