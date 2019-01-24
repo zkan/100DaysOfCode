f3b90665f976701a58fd52bb6fbce9d9df198f6a
zkan <kan@prontomarketing.com>
2019-01-24 07:13:44 +0700

    Use MIME for emailing :bear:

---
+++ python/emailer/emailer-mime.py
@@ -0,0 +1,41 @@
+import os
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+from_addr = 'kan@prontomarketing.com'
+to_addr = 'kan@prontomarketing.com'
+
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = 'New Releases and Sales on Steam'
+
+body = '''
+New Releases and Sales on Steam.
+
+Click the links below to check them out!
+'''
+msg.attach(MIMEText(body, 'plain'))
+
+smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+
+# heartbeat with smtp server
+smtp_server.ehlo()
+
+# start encryption session
+smtp_server.starttls()
+
+smtp_server.login('kan@prontomarketing.com',
+                  os.environ.get('GOOGLE_APP_PASSWORD'))
+
+text = msg.as_string()
+print(text)
+
+smtp_server.sendmail(from_addr, to_addr, text)
+
+smtp_server.quit()
+
+print('Email sent successfully')
