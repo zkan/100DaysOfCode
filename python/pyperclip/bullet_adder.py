da06e137ba5f2eeaeaed71acb34c8d60e1ae57c5
zkan <kan@prontomarketing.com>
2019-01-28 07:09:14 +0700

    Get text from clipboard and prepend * to each line :bear:

---
+++ python/pyperclip/bullet_adder.py
@@ -0,0 +1,12 @@
+import pyperclip
+
+
+text = pyperclip.paste()
+
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
