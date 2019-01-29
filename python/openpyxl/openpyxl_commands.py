9e119385f8324f98a74e8ff120e12aad42fc328a
zkan <kan@prontomarketing.com>
2019-01-29 07:13:29 +0700

    Get values from cells and do some math :bear:

---
+++ python/openpyxl/openpyxl_commands.py
@@ -0,0 +1,32 @@
+from openpyxl import load_workbook
+
+
+wb = load_workbook('financial_sample.xlsx')
+
+print(wb.sheetnames)
+
+# Select the last active worksheet (last sheet we edited)
+# if we have more than one worksheets
+ws1 = wb.active
+
+ws2 = wb['Yearly Totals']
+print(ws2)
+
+ws1 = wb['Finances 2017']
+print(ws1)
+
+print(ws1['C9'].value)
+print(ws1['B9'].value)
+
+profit_total = 0
+for index, row in enumerate(ws1['L']):
+    if index == 0:
+        continue
+
+    if index == 100:
+        break
+
+    profit_total += float(row.value)
+
+print(profit_total)
+
