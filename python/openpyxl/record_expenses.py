bf195a3e73f3867af6eeb3f3bee67e0f8750bfd2
zkan <kan@prontomarketing.com>
2019-01-31 08:11:37 +0700

    Sum expenses + save cost as float :bear:

--- python/openpyxl/record_expenses.py
+++ python/openpyxl/record_expenses.py
@@ -12,6 +12,9 @@ except FileNotFoundError:
 
 item = input('What do you buy? ')
 cost = input('How much? ')
-ws1.append([date.today(), item, cost])
+ws1.append([date.today(), item, float(cost)])
+max_row = ws1.max_row
+
+ws1[f'D1'] = f'=SUM(C1:C{max_row})'
 
 wb.save('expenses.xlsx')
