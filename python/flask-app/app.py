af66e79e761f6837ab42626c8fb7650644877721
zkan <kan@prontomarketing.com>
2019-02-05 06:57:27 +0700

    Get data and render in table :bear:

--- python/flask-app/app.py
+++ python/flask-app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 
+from data import fav_beer
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           fav_beer=fav_beer)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
