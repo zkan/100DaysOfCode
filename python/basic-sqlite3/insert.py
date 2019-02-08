b0e83d4f0af43f170aa67ba150fb6f558b4102d0
zkan <kan@prontomarketing.com>
2019-02-08 07:39:07 +0700

    Use context instead :bear:

--- python/basic-sqlite3/insert.py
+++ python/basic-sqlite3/insert.py
@@ -1,15 +1,8 @@
 import sqlite3
 
 
-# Create db if it doesn't exist
-connection = sqlite3.connect('addressbook.db')
-
-c = connection.cursor()
-
-c.execute("""INSERT INTO details
-VALUES('Kan', '123 Fake St, Thailand', 22222222)
-""")
-
-connection.commit()
-
-connection.close()
+with sqlite3.connect('addressbook.db') as connection:
+    c = connection.cursor()
+    c.execute("""INSERT INTO details
+    VALUES('Kan', '123 Fake St, Thailand', 22222222)
+    """)
