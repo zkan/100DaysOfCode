9b1e69e03460c422091839cea9c83370eb99dd69
zkan <kan@prontomarketing.com>
2019-02-08 07:41:52 +0700

    Write comment to let know we don't need connection commit :bear:

--- python/basic-sqlite3/insert.py
+++ python/basic-sqlite3/insert.py
@@ -3,6 +3,8 @@ import sqlite3
 
 with sqlite3.connect('addressbook.db') as connection:
     c = connection.cursor()
+
+    # We don't need to do connection.commit() when we use the context
     c.execute("""INSERT INTO details
     VALUES('Kan', '123 Fake St, Thailand', 22222222)
     """)
