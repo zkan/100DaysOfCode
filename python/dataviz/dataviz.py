7fd40db95ef58ee2afc13ee27a95c765a11ccc1b
zkan <kan@prontomarketing.com>
2019-02-10 07:37:21 +0700

    Implement convert and get category functions :bear:

--- python/dataviz/dataviz.py
+++ python/dataviz/dataviz.py
@@ -1,9 +1,29 @@
+from datetime import datetime
+import re
+
 import feedparser
 import plotly
 import plotly.graph_objs as go
 
 
-url = 'http://projects.bobbelderbos.com/pcc/dates/all.rss.xml'
-blog_feed = feedparser.parse(url)
-entries = blog_feed['entries']
-print(entries)
+def convert_to_datetime(date_str):
+    date_str = date_str.split('+')[0].strip()
+    dt = datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S')
+    return f'{dt.year}-{dt.month}'
+
+
+def get_category(link):
+    known = dict(codechallenge='challenge',
+                 twitter='news',
+                 special='special',
+                 geust='guest',)
+    default = 'article'
+    category = re.sub(r'.*\.es/([a-z]+).*', r'\1', link)
+    return known.get(category) or default
+
+
+if __name__ == '__main__':
+    url = 'http://projects.bobbelderbos.com/pcc/dates/all.rss.xml'
+    blog_feed = feedparser.parse(url)
+    entries = blog_feed['entries']
+    print(entries)
