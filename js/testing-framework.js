166f5555f9d0e7df82d022b1830de0d47e9d060d
zkan <kan@prontomarketing.com>
2019-04-08 09:29:46 +0700

    :recycle: Refactor test framewrok :bear:

--- js/testing-framework.js
+++ js/testing-framework.js
@@ -1,20 +1,16 @@
 const {sum, subtract} = require('./math')
 
-let result, expected
-
-function sumTest() {
-  result = sum(3, 7)
-  expected = 10
+test('sum adds numbers', () => {
+  const result = sum(3, 7)
+  const expected = 10
   expect(result).toBe(expected)
-}
-test('sum adds numbers', sumTest)
+})
 
-function subtractTest() {
-  result = subtract(7, 3)
-  expected = 4
+test('subtract subtracts numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
   expect(result).toBe(expected)
-}
-test('subtract subtracts numbers', subtractTest)
+})
 
 function test(title, callback) {
   try {
