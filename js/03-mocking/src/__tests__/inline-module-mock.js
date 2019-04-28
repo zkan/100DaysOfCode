554d1ff28e596bf3ce57df077eb133b5330e8ecf
zkan <kan@prontomarketing.com>
2019-04-28 08:01:01 +0700

    Use jest.mock to mock module :bear:

---
+++ js/03-mocking/src/__tests__/inline-module-mock.js
@@ -0,0 +1,30 @@
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
+jest.mock('../utils', () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p1)
+  }
+})
+
+test('returns winner', () => {
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+  expect(winner).toBe('Kent C. Dodds')
+  expect(utils.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler']
+  ])
+  expect(utils.getWinner).toHaveBeenCalledTimes(2)
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    1,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    2,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+
+  utils.getWinner.mockReset()
+})
