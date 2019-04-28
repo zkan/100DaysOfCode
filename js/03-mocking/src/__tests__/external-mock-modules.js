dff2fb4c8b9558ee7cb1bc03f88e4bee50977680
zkan <kan@prontomarketing.com>
2019-04-28 08:01:22 +0700

    Put mock modules in __mocks__ and jest will pick them up automatically :bear:

---
+++ js/03-mocking/src/__tests__/external-mock-modules.js
@@ -0,0 +1,26 @@
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
+jest.mock('../utils')
+
+test('returns winner', () => {
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+  expect(winner).toBe('Kent C. Dodds')
+  expect(utils.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler']
+  ])
+  expect(utils.getWinner).toHaveBeenCalledTimes(2)
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    1,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    2,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+
+  utils.getWinner.mockReset()
+})
