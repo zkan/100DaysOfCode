d98d1899245a0845dfba77895cfcd3b4aeb4b9ac
zkan <kan@prontomarketing.com>
2019-05-02 13:18:32 +0700

    Add console.log (commented) for testing running test in browser environment :bear:

--- js/04-jest/src/__tests__/utils.js
+++ js/04-jest/src/__tests__/utils.js
@@ -3,3 +3,6 @@ import { getFormattedValue } from '../utils'
 test('formats the value', () => {
   expect(getFormattedValue('1234.0')).toBe('1,234.0')
 })
+
+// To be able to get window object, enable jest-environment-jsdom
+//console.log(window)
