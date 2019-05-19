45ab04baff127088612446232a5b6f4274969b55
zkan <kan@prontomarketing.com>
2019-05-19 07:30:29 +0700

    Use template to render HTML :bear:

--- python/flask-intro/program/routes.py
+++ python/flask-intro/program/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
+
 from program import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello Pythonistas'
+    return render_template('index.html')
