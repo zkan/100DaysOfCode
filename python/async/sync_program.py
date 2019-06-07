f996d3780f3b736dcb6a56275554c68ffd4c0421
zkan <kan@prontomarketing.com>
2019-06-07 08:01:41 +0700

    :recycle: Move some logic into function and calculate total time :bear:

--- python/async/sync_program.py
+++ python/async/sync_program.py
@@ -1,13 +1,28 @@
+import datetime
 import random
 import time
 
 import colorama
 
 
-def process():
-    print(colorama.Fore.CYAN + 'Processing..')
+def process(number):
+    print(colorama.Fore.CYAN + f'Processing.. {number}')
     time.sleep(random.random() + .5)
 
 
-for _ in range(10):
-    process()
+def run(number):
+    for _ in range(10):
+        process(number)
+
+
+t0 = datetime.datetime.now()
+
+run(1)
+run(2)
+
+dt = datetime.datetime.now() - t0
+
+print(
+    colorama.Fore.WHITE + f'App exiting, total time: {dt.total_seconds()}',
+    flush=True
+)
